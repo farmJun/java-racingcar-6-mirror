d989a623ea2818b8d9e8c07d5f180ce1adc398b5
박준영 <wnsdud6969@naver.com>
2023-12-13 00:12:57 +0900

    test(NameTest): Name을 테스트하다
    - 유효한 길이의 이름을 생성한다
    - 유효한 길이로 이름을 생성하면 예외가 발생한다.

---
+++ src/test/java/racingcar/domain/NameTest.java
@@ -0,0 +1,24 @@
+package racingcar.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class NameTest {
+
+    @DisplayName("유효한 길이의 이름을 생성한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "ab", "abc", "abcd"})
+    void createNameByValidNameLength(String input) {
+        assertDoesNotThrow(() -> new Name(input));
+    }
+
+    @DisplayName("유효한 길이로 이름을 생성하면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"", "abcdef"})
+    void createNameByInvalidNameLength(String input) {
+        assertThrows(IllegalArgumentException.class, () -> new Name(input));
+    }
+}
