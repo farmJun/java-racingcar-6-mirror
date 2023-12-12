d28a8ffa52cfe5f577b8d3233828c556b16839c3
박준영 <wnsdud6969@naver.com>
2023-12-12 21:53:04 +0900

    refactor(DrivingDistance): 생성자 리팩터링
    - String 타입의 이름을 매개변수로 받아 자동차 이름 생성
    - 이동 거리는 자동차의 초기 이동 거리로 생성

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -4,12 +4,14 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Car {
 
+    private static final int initialDrivingDistance = 0;
+
     private final Name carName;
     private final DrivingDistance drivingDistance;
 
-    public Car(Name carName, DrivingDistance drivingDistance) {
-        this.carName = carName;
-        this.drivingDistance = drivingDistance;
+    public Car(String carName) {
+        this.carName = new Name(carName);
+        this.drivingDistance = new DrivingDistance(initialDrivingDistance);
     }
 
     public void attemptToMove() {
--- src/main/java/racingcar/domain/DrivingDistance.java
+++ src/main/java/racingcar/domain/DrivingDistance.java
@@ -2,12 +2,7 @@ package racingcar.domain;
 
 public class DrivingDistance {
 
-    private static final int initialDrivingDistance = 0;
     private int drivingDistance;
-
-    public DrivingDistance() {
-        this.drivingDistance = initialDrivingDistance;
-    }
 
     public DrivingDistance(int drivingDistance) {
         this.drivingDistance = drivingDistance;
