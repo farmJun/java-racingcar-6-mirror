039af7e08463c77c33d767b0ba837bc0f85bd488
박준영 <wnsdud6969@naver.com>
2023-12-12 21:34:36 +0900

    feat(Name): Name을 구현하다
    - 생성자를 추가하다
    - 이름의 유효성을 검증하다

---
+++ src/main/java/racingcar/domain/Name.java
@@ -0,0 +1,17 @@
+package racingcar.domain;
+
+public class Name {
+
+    private final String name;
+
+    public Name(String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    private void validate(String name) {
+        if (name.length() > 5 || name.length() < 1) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
