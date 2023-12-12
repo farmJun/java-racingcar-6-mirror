e14b2484b2e10a94fd0d6502a46545a06cc6e90e
박준영 <wnsdud6969@naver.com>
2023-12-12 23:47:54 +0900

    feat(Winner): Winner를 구현하다
    - 생성자를 구현하다
    - 우승자의 이름을 반환한다

---
+++ src/main/java/racingcar/domain/Winner.java
@@ -0,0 +1,15 @@
+package racingcar.domain;
+
+public class Winner {
+
+    private final Name winnerName;
+
+    public Winner(Name winnerName) {
+        this.winnerName = winnerName;
+    }
+
+    public Name getWinnerName() {
+        return winnerName;
+    }
+
+}
